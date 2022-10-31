f08cc2a352812bd027088b1e67f59a9285343a2a
lucassimon2021 <luuucassimon2019@gmail.com>
2022-10-31 20:19:18 -0300

    arreglos

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ class Producto{
     }
 }
 
-const Computadora =new Producto(1,"Computadora", 1000)
-const Celular=new Producto(2,"Celular",2200)
-const Televisor=new Producto(3,"Televisor",800)
-const Tablet=new Producto(4,"Tablet",500)
-//Esta funcion sirve para sumar productos al carrito y almacena los items y el valor total de la compra
+const Computadora =new Producto(1,"Computadora", 153000)
+const Celular=new Producto(2,"Celular",62000)
+const Televisor=new Producto(3,"Televisor",180000)
+const Tablet=new Producto(4,"Tablet",52000)
+
+
 function agregarCarrito (){
-    choise=parseInt(prompt(`Elija un producto: ${Computadora.identificacion}. ${Computadora.nombre},${Celular.identificacion}. ${Celular.nombre}, ${Televisor.identificacion}. ${Televisor.nombre},${Tablet.identificacion}. ${Tablet.nombre}`))
+    choise=parseInt(prompt(`Elije tu producto: ${Computadora.identificacion}. ${Computadora.nombre},${Celular.identificacion}. ${Celular.nombre}, ${Televisor.identificacion}. ${Televisor.nombre},${Tablet.identificacion}. ${Tablet.nombre}`))
     while(seguirComprando===true){
         if (choise===Computadora.identificacion){
             carrito=carrito + Computadora.nombre+", "
@@ -29,35 +30,34 @@ function agregarCarrito (){
         }else{
             choise=prompt("Por favor ingresa un producto correcto")
         }
-        decision = parseInt(prompt("desea sieguir comprando 1.Si 2.No"))
+        decision = parseInt(prompt("desea seguir comprando 1.Si, claro. 2.No, es todo."))
         if(decision===1){
             choise = parseInt(prompt(`Elija un producto: ${Computadora.identificacion}. ${Computadora.nombre},${Celular.identificacion}. ${Celular.nombre}, ${Televisor.identificacion}. ${Televisor.nombre},${Tablet.identificacion}. ${Tablet.nombre}`))
         }else if(decision===2){
             seguirComprando=false
         }else{
-            decision=parseInt(prompt("desea sieguir comprando 1.Si 2.No"))
+            decision=parseInt(prompt("desea siguir comprando 1.Si, claro. 2.No, es todo"))
         }
     }
 }
-//esta funcion le da la opcion al comprador determinar el metodo de pago a elegir y devuelve un Alert con el resumen de la compra
+
+
 function cuotas(){
-    modoDePago = parseInt(prompt(`tu total es ${total}, como deseas pagarlo 1. efectivo 10% de descuento-${total*0.9},2.en un pago por trasnferencia o tarjeta ${total} 3. 3 cuotas sin interes de ${total/3}, 4. 12 cuotas con interes de${(total*1.5)/12}.`))
+    modoDePago = parseInt(prompt(`Tu total es $${total}, ¿Como deseas pagarlo? 1. En Efectivo con un 10% de descuento, su total es: $${total*0.9} 2. En un pago por transferencia o tarjeta, su total es: $${total} 3. En 3 cuotas sin interes de $${total/3} cada una. 4. En 12 cuotas con interes de $${(total*1.4)/12} cada una.`))
         if (modoDePago===1){
-            pagos = "un pago en efectivo de $ "+total*0.9
+            pagos = "un pago en efectivo de $"+total*0.9
         }else if (modoDePago===2){
-            pagos = "un pago de $ "+ total
+            pagos = "un pago de $"+ total
         }else if (modoDePago===3){
-            pagos = "3 cuotas de $ " +total/3
+            pago = "3 cuotas de $" + total/3
         }else if (modoDePago===4){
-            pagos = "12 cuotas de $"+(total*1.5)/12
+            pagos = "12 cuotas de $"+(total*1.4)/12
         }else{
-            modoDePago=prompt("Por favor ingresa un metodo de pago correcto")
+            modoDePago=prompt("El metodo no es correcto, Por favor ingresa un metodo de pago correcto")
             pago=true
         }
-    alert(`Usted acaba de comprar ${carrito}, por un total de: $${total} su metodo de pago es:${pagos}`)
+    alert(`Usted acaba de comprar ${carrito} por un total de: $${total}`)
 }
-
-//inicio del programa
 
 let carrito = " "
 let total =0
@@ -66,4 +66,3 @@ let pagos
 
 agregarCarrito()
 cuotas()
-alert("Muchas gracias por su compra vuelva pronto")
