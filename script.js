65c4a40a8ed8155ff4e1aef6d87ebe750cfd40d7
lucassimon2021 <luuucassimon2019@gmail.com>
2022-11-27 15:15:46 -0300

    primeros

--- script.js
+++ script.js
@@ -1,68 +1,156 @@
-class Producto{
-    constructor(id,nombre,precio){
-        this.id=id,
-        this.nombre=nombre,
-        this.precio=precio
+class Producto {
+    constructor(id, nombre, precio, stock) {
+      this.id = id;
+      this.nombre = nombre;
+      this.precio = precio;
+      this.stock = stock;
     }
-}
-
-const RemeraF1 = new Producto(1,"RemeraF1", 1500)
-const GorraF1 = new Producto(2,"GorraF1",900)
-const BuzoF1 = new Producto(3,"BuzoF1",3500)
-const TazaF1 = new Producto(4,"TazaF1",520)
-
-
-function agregarCarro (){
-    choise=parseInt(prompt(`Elije tu producto: ${RemeraF1.id}. ${RemeraF1.nombre},${GorraF1.id}. ${GorraF1.nombre}, ${BuzoF1.id}. ${BuzoF1.nombre},${TazaF1.id}. ${TazaF1.nombre}`))
-    while(seguirComprando===true){
-        if (choise===RemeraF1.id){
-            carro=carro + RemeraF1.nombre+", "
-            total= total+ RemeraF1.precio
-        }else if (choise===GorraF1.id){
-            carro=carro + GorraF1.nombre+", "
-            total= total+ GorraF1.precio
-        }else if (choise===BuzoF1.id){
-            carro=carro + BuzoF1.nombre+", "
-            total= total+ BuzoF1.precio
-        }else if (choise===TazaF1.id){
-            carro=carro + TazaF1.nombre+", "
-            total= total+ TazaF1.precio
-        }else{
-            choise=prompt("Por favor ingresa un producto correcto")
-        }
-        decision = parseInt(prompt("desea seguir comprando 1.Si, claro. 2.No, es todo."))
-        if(decision===1){
-            choise = parseInt(prompt(`Elija un producto: ${RemeraF1.id}. ${RemeraF1.nombre},${GorraF1.id}. ${GorraF1.nombre}, ${BuzoF1.id}. ${BuzoF1.nombre},${TazaF1.id}. ${TazaF1.nombre}`))
-        }else if(decision===2){
-            seguirComprando=false
-        }else{
-            decision=parseInt(prompt("desea siguir comprando 1.Si, claro. 2.No, es todo"))
-        }
+    restaStock() {
+      this.stock = --this.stock;
+      console.log(
+        `Gracias por comprar tu ${this.nombre}, hemos actualizado el Stock`
+      );
     }
-}
+  }
+  
+  const producto1 = new Producto(0, "RemeraF1", 1500, 20);
+  const producto2 = new Producto(1, "GorraF1", 900, 50);
+  const producto3 = new Producto(2, "BuzoF1", 3500, 15);
+  const producto4 = new Producto(3, "TazaF1", 520, 100);
+  
+  const productos = [producto1, producto2, producto3, producto4];
+  const carrito = [];
+  const numeros = [1, 2 ,3];
+ 
+  
+  // Creamos el prompt para que seleccione el producto:
+  
+  let productosOfrecidos = "Tenemos para ofrecerle: (99 para Finalizar) \n";
+  
+  // Creamos la función que va a recorrer nuestros productos para que mostrarlos en el prompt
+  
+  function agregaCarrito() {
+    productos.forEach((producto) => {
+      productosOfrecidos += `
+      ${producto.id}) ${producto.nombre} a: $${producto.precio}
+      `;
+    });
+  
+    // Guardamos lo ingresado en un prompt
+  
+    let respuesta = parseInt(prompt(productosOfrecidos));
+  
+    // Creamos un ciclo que evalue si es un numero y en el caso de q no lo sea le volvemos a mostrar el prompt
+    while (isNaN(respuesta)) {
+      alert("Ingrese solo numeros");
+      let respuesta = parseInt(prompt(productosOfrecidos));
+    }
+  
+    while (respuesta != 99) {
+      switch (respuesta) {
+        case 0:
+          carrito.push(productos[0]);
+          alert(`agregamos al carrito el producto ${productos[0].nombre}`);
+          break;
+        case 1:
+          carrito.push(productos[1]);
+          alert(`agregamos al carrito el producto ${productos[1].nombre}`);
+          break;
+        case 2:
+          carrito.push(productos[2]);
+          alert(`agregamos al carrito el producto ${productos[2].nombre}`);
+          break;
+        case 3:
+          carrito.push(productos[3]);
+          alert(`agregamos al carrito el producto ${productos[3].nombre}`);
+          break;
+  
+        default:
+          alert("No tenemos el producto que elegiste");
+          break;
+      }
+      respuesta = parseInt(prompt(productosOfrecidos));
+    }
+    alert("Cerramos tu pedido");
+    mostrarCarrito();
+  }
+
+  
+  //
+  let productosCarrito = "Vas a llevar:";
+  let precioCarrito = 0;
+  agregaCarrito();
+  
+  function mostrarCarrito() {
+    carrito.forEach((itemsElegidos) => {
+      productosCarrito += `\n - ${itemsElegidos.nombre}`;
+      precioCarrito += itemsElegidos.precio;
+    });
+  
+    alert(
+      `Repasemos \n ${productosCarrito} \n por un total de: $${precioCarrito}`
+    );
+  }
+
+  // find()
+  const buscarproducto = productos.find((e) => e.id === 1);
+  alert(
+    `En tu proxima compra usando el codigo: codfalso. Tienes un 50% de descuento en ${buscarproducto.nombre}.`
+  );
 
 
-function cuotas(){
-    modoDePago = parseInt(prompt(`Tu total es $${total}, ¿Como deseas pagarlo? 1. En Efectivo con un 10% de descuento, su total es: $${total*0.9} 2. En un pago por transferencia o tarjeta, su total es: $${total} 3. En 3 cuotas sin interes de $${total/3} cada una. 4. En 12 cuotas con interes de $${(total*1.4)/12} cada una.`))
-        if (modoDePago===1){
-            pagos = "un pago en efectivo de $"+total*0.9
-        }else if (modoDePago===2){
-            pagos = "un pago de $"+ total
-        }else if (modoDePago===3){
-            pago = "3 cuotas de $" + total/3
-        }else if (modoDePago===4){
-            pagos = "12 cuotas de $"+(total*1.4)/12
-        }else{
-            modoDePago=prompt("El metodo no es correcto, Por favor ingresa un metodo de pago correcto")
-            pago=true
-        }
-    alert(`Usted acaba de comprar ${carro} por un total de: $${total}`)
-}
 
-let carro = " "
-let total =0
-let seguirComprando= true
-let pagos
 
-agregarCarro()
-cuotas()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
